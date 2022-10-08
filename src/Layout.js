428a60962740ef35e9b106bd82d7208ad97284b2
chaitanyantr <32541042+chaitanyantr@users.noreply.github.com>
2022-10-08 16:28:44 -0400

    src - code - main

---
+++ src/Layout.js
@@ -0,0 +1,132 @@
+export const layout = {
+    canvas : {
+          height: "800",
+          width: "800",
+      },
+    
+    Appmain : {
+      backgroundColor: "#eeeeee",
+      height: "100vh",
+      width: "100vw",
+    },
+
+    text: { 
+        position: "absolute",
+        left:900,
+        top:20,
+        color:"red",
+        backgroundColor: "#eeeeee",
+        width:500,
+        fontSize: 70
+      },
+
+    victory: { 
+        position: "absolute",
+        left:50,
+        top:325,
+        color:"red",
+        // backgroundColor: "#320453",
+        width:500,
+        fontSize: 30
+      },
+
+    buttons: { 
+        position: "absolute",
+        left: 950,
+        top:90
+      },
+
+      upbutton:  {
+        position: "absolute",
+        left: 70,
+        // color:"red",
+        top: 390,
+        width: 100,
+        height:50,
+        backgroundColor:"#55F925",
+        cursor:"pointer",
+      },
+      
+      downbutton : {
+        position: "absolute",
+        backgroundColor:"#55F925",
+        left: 70,
+        width: 100,
+        height:50,
+        top: 510,
+        cursor:"pointer",
+      },
+      
+      leftbutton : {
+        position: "absolute",
+        backgroundColor:"#55F925",
+        width: 100,
+        height:50,
+        top: 450,
+        cursor:"pointer",
+      },
+      
+      rightbutton : {
+        position: "absolute",
+        backgroundColor:"#55F925",
+        width: 100,
+        height:50,
+        top: 450,
+        left: 150,
+        cursor:"pointer",
+
+      },
+      solvebutton : {
+        position: "absolute",
+        backgroundColor:"#13e8f0",
+        width: 150,
+        height:50,
+        top: 600,
+        left: 0,
+        cursor:"pointer",
+
+      },
+
+      resetbutton : {
+        position: "absolute",
+        backgroundColor:"#13e8f0",
+        width: 150,
+        height:50,
+        top: 600,
+        left: 200,
+        cursor:"pointer",
+
+      },
+
+
+
+      configOne:{
+        position: "absolute",
+        width: 150,
+        height:50,
+        cursor:"pointer",
+        top: 30,
+        left: 100,
+        backgroundColor:"#e6f518"
+        
+      },
+      configTwo:{
+        position: "absolute",
+        cursor:"pointer",
+        width: 150,
+        height:50,
+        top: 130,
+        left: 100,
+        backgroundColor:"#e6f518"
+      },
+      configThree:{
+        position: "absolute",
+        cursor:"pointer",
+        width: 150,
+        height:50,
+        top: 230,
+        left: 100,
+        backgroundColor:"#e6f518"
+      },
+
+}
